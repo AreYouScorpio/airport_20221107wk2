046b10dba1b5b7e7643124df96feb91c1906c8d6
AreYouScorpio <akos.schweger@gmail.com>
2022-11-07 11:23:08 +0100

    repo loggolas AOP added and working

--- src/main/java/hu/webuni/airport/aspect/LoggingAspect.java
+++ src/main/java/hu/webuni/airport/aspect/LoggingAspect.java
@@ -1,4 +1,21 @@
 package hu.webuni.airport.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+
+    @Before("execution(* hu.webuni.airport.repository.*.*(..))") //* mind1 melyik tipusban (barmely interface, vagy osztaly), * mindegy, h mi a metodus (barmely metodus), (..) mindegy, milyen parameterei vannak
+    public void logBefore(JoinPoint joinPoint){
+        System.out.println(String.format("Method %s called in class %s",
+                joinPoint.getSignature(), //.getSignature melyik metodus, teljes signaturaja a metodusnak
+                joinPoint.getTarget().getClass().getName() //.getTarget a cel osztaly, megkerdezzuk oszt nevet
+                ));
+    }
+
 }
