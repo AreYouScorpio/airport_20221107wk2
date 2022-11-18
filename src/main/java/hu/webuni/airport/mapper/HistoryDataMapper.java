08697d52a93207b4471eaae32a9378229ed113be
AreYouScorpio <akos.schweger@gmail.com>
2022-11-18 11:08:05 +0100

    open api generator .. HistoryDataMapper repaired

--- src/main/java/hu/webuni/airport/mapper/HistoryDataMapper.java
+++ src/main/java/hu/webuni/airport/mapper/HistoryDataMapper.java
@@ -3,8 +3,9 @@ package hu.webuni.airport.mapper;
 import hu.webuni.airport.api.model.HistoryDataAirportDto;
 import hu.webuni.airport.model.Airport;
 import hu.webuni.airport.model.HistoryData;
+import org.mapstruct.Mapper;
 
-public class HistoryDataMapper {
-    public HistoryDataAirportDto airportHistoryDataToDto(HistoryData<Airport> airportHistoryData) {
-    }
+@Mapper(componentModel = "spring")
+public interface HistoryDataMapper {
+     HistoryDataAirportDto airportHistoryDataToDto(HistoryData<Airport> airportHistoryData);
 }
