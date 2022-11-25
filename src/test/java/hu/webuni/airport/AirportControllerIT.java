f2fa2ba9a5232135c415b1e5b2986b74b3f63300
AreYouScorpio <akos.schweger@gmail.com>
2022-11-25 16:35:25 +0100

    GraphNumberTest :) for contest

--- src/test/java/hu/webuni/airport/AirportControllerIT.java
+++ src/test/java/hu/webuni/airport/AirportControllerIT.java
@@ -1,8 +1,12 @@
 package hu.webuni.airport;
 
+
 import hu.webuni.airport.api.model.AirportDto;
 import hu.webuni.airport.api.model.AirportDto;
+import hu.webuni.airport.service.AirportService;
+import hu.webuni.airport.service.InitDbService;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,6 +17,8 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // server indítás random porton
@@ -79,4 +85,18 @@ public class AirportControllerIT {
 
         return responseList;
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
